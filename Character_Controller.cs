f8f9277d415433cb5d7f9a5e73dc31f5f07f6282
BomBymBom <popovicisandu122@gmail.com>
2021-05-17 22:48:23 +0300

    Create Character_Controller.cs

---
+++ Character_Controller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpPower;
+
+    private float _gravityForce;
+    private Vector3 _moveVector;
+
+    private CharacterController _controller;
+    private Animator _animator;
+
+    private void Start(){
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update(){
+        CharacterMove();
+        PlayerGravity();
+    }
+
+    private void CharacterMove(){
+        if(_controller.isGrounded){
+            _animator.ResetTrigger("Jump");
+            _moveVector = Vector3.zero;
+            _moveVector.x = Input.GetAxis("Horizontal")* speed;
+            _moveVector.z = Input.GetAxis("Vertical")* speed;
+
+
+            if(Vector3.Angle(Vector3.forward,_moveVector)>1f || Vector3.Angle(Vector3.forward,_moveVector) == 0){
+                Vector3 direct = Vector3.RotateTowards(transform.forward, _moveVector, speed, 0.0f);
+                transform.rotation = Quaternion.LookRotation(direct);
+            }
+        }
+            if(_moveVector.x!=0 && _controller.isGrounded || _moveVector.z!=0 && _controller.isGrounded )_animator.SetBool("Move", true);
+            else _animator.SetBool("Move", false);
+       
+        _moveVector.y = _gravityForce;
+
+        _controller.Move(_moveVector * Time.deltaTime);
+        
+    }
+
+    private void PlayerGravity(){
+        if(!_controller.isGrounded) _gravityForce -=60f * Time.deltaTime;
+        else _gravityForce = -1f;
+
+        if(Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded){
+             _gravityForce = jumpPower;
+            _animator.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Weapon_Controller.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Controller : MonoBehaviour
+{
+    private GameObject _objWeapon;
+    private Animator _weapAnimator;
+
+    private bool _usingWeapon = false;
+    private string _weaponName;
+
+    private bool _isTuching = false;
+
+
+
+    private void Start()
+    {
+        _weapAnimator = GetComponent<Animator>();
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (_usingWeapon && Input.GetKeyDown(KeyCode.G))
+        {
+            _weapAnimator.SetTrigger("Put Down");
+        }
+
+        else if (!_usingWeapon && Input.GetKeyDown(KeyCode.F) && _isTuching)
+        {
+            _weapAnimator.SetTrigger("Pick Up");
+        }
+
+    }
+    // Animation 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && _usingWeapon && _objWeapon.name == "Shield")
+        {
+            _weapAnimator.SetTrigger("Shield Attack");
+        }
+        else if (Input.GetMouseButtonDown(0) && _usingWeapon && _objWeapon.name == "Sword")
+        {
+            _weapAnimator.SetTrigger("Sword Attack");
+        }
+        else if (Input.GetMouseButtonDown(0) && _usingWeapon && _objWeapon.name == "Axe")
+        {
+            _weapAnimator.SetTrigger("Axe Attack");
+        }
+    }
+
+
+    // Invoked in animation
+    private void TakeWeapon()
+    {
+        _objWeapon = GameObject.Find(_weaponName);
+        _objWeapon.transform.parent = _objWeapon.GetComponent<WeaponPossition>().Hand.transform;
+        _objWeapon.transform.localPosition = _objWeapon.GetComponent<WeaponPossition>().PickPosition;
+        _objWeapon.transform.localEulerAngles = _objWeapon.GetComponent<WeaponPossition>().PickRotation;
+        _usingWeapon = true;
+        _isTuching = false;
+        _weapAnimator.ResetTrigger("Pick Up");
+    }
+    // Invoked in animation
+    private void DropWeapon()
+    {
+
+        _objWeapon.transform.parent = null;
+        _objWeapon.transform.localPosition = new Vector3(_objWeapon.transform.position.x, 1, _objWeapon.transform.position.z);
+        _objWeapon.transform.localEulerAngles = new Vector3(-90, _objWeapon.transform.position.y, _objWeapon.transform.position.z);
+        _usingWeapon = false;
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            _isTuching = true;
+            _weaponName = other.gameObject.name;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Weapon"))
+        {
+            _isTuching = false;
+        }
+    }
+
+}
